5dadedcb03f8a299e5ea467d9211d708288b3e28
Darkrany <101005069+Darkrany@users.noreply.github.com>
2023-03-27 20:18:25 +0200

    array y promises
    recorremos todos los pokemon y añadimos promise.all para gestionar la cadena de promesas

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 const fetchPokemon = () => {
-    const api = `https://pokeapi.co/api/v2/pokemon/1`; /* llamamos URL api */
-    fetch(api)
-        .then((res) => {        /* promesa asincrona para recibir info de api y convetir a JSON */
-            return res.json();  
-        })
-        .then((data) => {       /* promesa recibe los datos */
-           
-            const pokemon = { /* creamos objeto y asignanos valores a propiedades */
+    const promises = []; /* array que almacenaremos promesas */
+
+    for (let i = 1; i <= 150; i++) { /* recoremos todos los pokemon */
+
+        const api = `https://pokeapi.co/api/v2/pokemon/${i}`; /* llamamos URL api */
+        promises.push(           /* almacenamos resultado petición promesa en array */
+                     fetch(api)
+                     .then((res) =>  res.json()    /* promesa asincrona para recibir info de api y convetir a JSON */
+            )
+        )
+    }
+    
+    
+    /* cuando todas las promesas se cumplen y recibimos los datos se reasignan en otro array */
+       Promise.all(promises).then(results => {
+        const pokemon = results.map((data) => (
+            
+            {   /* creamos objeto y asignanos valores a propiedades */
                 name: data.name,
                 id: data.id,
                 img: data.sprites['front_default'],
                 type: data.types.map((type)=> type.type.name) .join(', ') 
 
                 /* Los tipos los recibimos en un array por lo que generamos otro solo con nombres */
-            }; 
+            })); 
 
-            
-            console.log(pokemon); 
-        });
+            console.log(pokemon)
+        
+        })
+       
 };
 
 fetchPokemon();
